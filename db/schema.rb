6a54993e71f1d15882339cc475b75d473082bb52
Minh Tu <minhtu12394@gmail.com>
2017-12-04 20:08:49 +0700

    login-logout

--- db/schema.rb
+++ db/schema.rb
@@ -81,8 +81,9 @@ ActiveRecord::Schema.define(version: 20171127130050) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
-    t.string "password_digest"
     t.string "name"
+    t.string "email"
+    t.string "password_digest"
     t.string "address"
     t.string "phone"
     t.boolean "gender"
