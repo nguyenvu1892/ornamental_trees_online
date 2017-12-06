f712abcbc78f5b9dc6807f606449855a41b2475d
vuducduy <vuducduy07071998@gmail.com>
2017-12-06 19:55:43 +0700

    Add_layout_home

---
+++ app/assets/javascripts/customjs.js
@@ -0,0 +1,5 @@
+//Static_Pages
+
+$(document).ready(function () {
+    $('.slider').slider({height: 720});
+});
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
+
+  get "static_pages/about"
   get    "/login",   to: "sessions#new"
   post    "/login",   to: "sessions#create"
   delete  "/logout",  to: "sessions#destroy"
+
+  root to: "static_pages#home"
 end
