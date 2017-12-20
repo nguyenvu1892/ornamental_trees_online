05a2ec81ce9e812ae1079f885b504dabb951c9d1
Minh Tu <minhtu12394@gmail.com>
2017-12-20 15:55:34 +0700

    login with user and admin

--- app/controllers/admin/admin_controller.rb
+++ app/controllers/admin/admin_controller.rb
@@ -1,6 +1,16 @@
 class Admin::AdminController < ApplicationController
+  before_action :logged_in_user
+
   def index
     @user = User.paginate page: params[:page]
     @products = Product.paginate page: params[:page]
   end
+
+  private
+    def logged_in_user
+      unless logged_in?
+        flash[:danger] =  t ".pls_login"
+        redirect_to login_url
+      end
+    end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,13 @@ class SessionsController < ApplicationController
     user = User.find_by email: params[:session][:email].downcase
     if user && user.authenticate(params[:session][:password])
       log_in user
-      redirect_to root_url
+      if user.admin?
+        redirect_to admin_root_url
+        flash[:admin] = t ".admin"
+      else
+        redirect_to root_url
+        flash[:user] = t ".user"
+      end
     else
       flash.now[:danger] = t ".danger"
       render :new
