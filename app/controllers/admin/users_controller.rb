5a4cd5c5ffaeae38762984cb7a2efdc4f472b39c
vuducduy <vuducduy07071998@gmail.com>
2017-12-27 19:05:37 +0700

    delete user

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,5 +1,6 @@
 class Admin::UsersController < ApplicationController
   before_action :logged_in_user
+  before_action :admin_user, only: [:destroy, :index]
 
   def index
     @user = User.all.paginate(page: params[:page])
@@ -9,24 +10,28 @@ class Admin::UsersController < ApplicationController
     @user = User.find_by id: params[:id]
     if @user.present?
       if @user.destroy
-        flash[:success] = t ".success"
+        flash[:success] = t "admin.users.destroy.success"
         redirect_to admin_users_url
       else
-        flash[:danger] = t ".danger"
+        flash[:danger] = t "admin.users.destroy.danger"
         redirect_to admin_users_url
       end
     else
-      flash[:danger] = t ".danger"
+      flash[:danger] = t "admin.users.destroy.danger"
       redirect_to admin_root_url
     end
   end
 
   private
-    def logged_in_user
-      unless logged_in?
-        flash[:danger] = "Please log in."
-        redirect_to login_url
-      end
-    end
 
+  def logged_in_user
+    unless logged_in?
+      flash[:danger] = t "admin.private.logged_in.pls_login"
+      redirect_to login_url
+    end
+  end
+
+  def admin_user
+    redirect_to(root_url) unless current_user.admin?
+  end
 end
