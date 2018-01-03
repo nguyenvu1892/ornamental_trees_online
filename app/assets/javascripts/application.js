d97182bde5552ef44f1e429e401fdaa021d12f10
Minh Tu <minhtu12394@gmail.com>
2018-01-03 14:40:39 +0700

    fix add product to cart

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,5 +14,4 @@
 //= require jquery_ujs
 //= require materialize
 //= require materialize-sprockets
-//= require rails-ujs
 //= require_tree .
--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,3 +1,7 @@
 class Cart < ApplicationRecord
+  belongs_to :product
 
+  scope :new_cart, -> session_id {
+    where(session_id: session_id)
+  }
 end
